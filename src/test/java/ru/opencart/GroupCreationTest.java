c58a63077f90490268819b7336ad04dddf27f2d0
Трофимова Анна Михайловна <TrofimovaAM@hq.corp.mos.ru>
2022-12-23 10:28:03 +0300

    update

--- src/test/java/ru/opencart/GroupCreationTest.java
+++ src/test/java/ru/opencart/GroupCreationTest.java
@@ -1,8 +1,10 @@
 package ru.opencart;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTest {
@@ -10,14 +12,24 @@ public class GroupCreationTest {
     ChromeDriver wd;
 
     @BeforeMethod
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("https://learn-qa.ru");
+        registration();
     }
 
-    @Test
-    public void positiveReg() {
-        wd.get("https://learn-qa.ru/index.php?route=account/register");
+    private void registration() {
+        gotoRegPage();
+        fillRegForm();
+        confirmRegForm();
+    }
+
+    private void confirmRegForm() {
+        wd.findElement(By.cssSelector(".btn-primary")).click();
+    }
+
+    private void fillRegForm() {
         wd.findElement(By.id("input-firstname")).click();
         wd.findElement(By.id("input-firstname")).sendKeys("Cthutq");
         wd.findElement(By.id("input-lastname")).click();
@@ -31,11 +43,32 @@ public class GroupCreationTest {
         wd.findElement(By.id("input-confirm")).click();
         wd.findElement(By.id("input-confirm")).sendKeys("123456");
         wd.findElement(By.name("agree")).click();
-        wd.findElement(By.cssSelector(".btn-primary")).click();
     }
+
+    private void gotoRegPage() {
+        wd.get("https://learn-qa.ru/index.php?route=account/register");
+    }
+
+
+    @Test
+    public void testPositiveRegistration() {
+
+        wd.get("https://learn-qa.ru/index.php?route=product/category&path=33");  //здесь должна быть проверка успешной регистрации
+    }
+
+
 
     @AfterMethod
     public void tearDown() {
         wd.quit();
+    }
+
+    public static boolean isAlertPresent(ChromeDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 }
